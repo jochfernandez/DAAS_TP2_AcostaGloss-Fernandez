974bb009e357297df245a4e65bcccf96b464d534
José Manuel Fernández <43698579@fi.unju.edu.ar>
2026-09-06 22:25:57 -0300

    Completando el TP2

--- src/main/java/ar/edu/unju/fi/arquitecturas/tp2/repository/TransaccionRepository.java
+++ src/main/java/ar/edu/unju/fi/arquitecturas/tp2/repository/TransaccionRepository.java
@@ -11,7 +11,6 @@ import java.util.UUID;
 @Repository
 public interface TransaccionRepository extends JpaRepository<Transaccion, UUID> {
 
-    // Query Method 5: Obtiene todo el historial de transacciones de una cuenta específica
     List<Transaccion> findByCuentaId(UUID cuentaId);
 
     // Query Method 6: Obtiene transacciones de una cuenta filtradas por tipo (ej. ver todos los "DEPOSITOS" de la cuenta X)
